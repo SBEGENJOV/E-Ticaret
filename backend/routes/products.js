3519e18f95b8145b82f73853e2fa62c29c564105
SBEGENJOV <seydi19981995@gmail.com>
2023-12-19 12:46:14 +0300

    Ürün işlemleri bitti

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -42,5 +42,46 @@ router.get("/:productId", async (req, res) => {
   }
 });
 
+//Ürün güncelleme(Update)
+router.put("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const updates = req.body;
 
+    const existingProduct = await Product.findById(productId);
+
+    if (!existingProduct) {
+      return res.status(404).json({ error: "Product not found." });
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      updates,
+      { new: true }
+    );
+    console.log(updatedProduct);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+
+//Ürün Silme(Delete)
+router.delete("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found." });
+    }
+    res.status(200).json(deletedProduct);
+    console.log(productId);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
 module.exports = router;
