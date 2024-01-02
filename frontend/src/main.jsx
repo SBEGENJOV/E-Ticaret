7190a8d22a8f46a038b769e729212b6911a414db
SBEGENJOV <seydi19981995@gmail.com>
2024-01-02 22:48:04 +0300

    sayfa üste gitme işlemi yapıldı

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import CartProvider from "./context/CartProvider.jsx";
-import {Layout} from "./layout/Layout.jsx"
+import { Layout } from "./layout/Layout.jsx";
 import App from "./App.jsx";
 import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-
+import ScrollToTop from "./ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <ScrollToTop />
     <CartProvider>
       <Layout>
         <App />
