3f5a6e958b581c300f607195f1baa7649eead9d6
SBEGENJOV <seydi19981995@gmail.com>
2024-01-02 04:13:27 +0300

    Ürün detay sayfası işlemleri yapıldı

--- frontend/src/component/Reviews/Reviews.jsx
+++ frontend/src/component/Reviews/Reviews.jsx
@@ -1,28 +1,36 @@
-import "./Reviews.css"
-import ReviewsForm from "./ReviewsForm"
-import ReviewsItem from "./ReviewsItem"
+import "./Reviews.css";
+import ReviewsForm from "./ReviewsForm";
+import ReviewsItem from "./ReviewsItem";
 import PropTypes from "prop-types";
 
-const Reviews = ({ active }) => {
+const Reviews = ({ active, singleProduct }) => {
   return (
     <div className={`tab-panel-reviews ${active}`}>
+      {singleProduct.reviews.length > 0 ? (
+        <>
           <h3>2 reviews for Basic Colored Sweatpants With Elastic Hems</h3>
           <div className="comments">
             <ol className="comment-list">
-            <ReviewsItem/>
-            <ReviewsItem/>
+              {singleProduct.reviews.map((item, index) => (
+                <ReviewsItem key={index} item={item} />
+              ))}
             </ol>
           </div>
-          <div className="review-form-wrapper">
-            <h2>Add a review</h2>
-           <ReviewsForm/>
-          </div>
-        </div>
-  )
-}
+        </>
+      ) : (
+        <h3>Hiç yorum yok...</h3>
+      )}
+      <div className="review-form-wrapper">
+        <h2>Add a review</h2>
+        <ReviewsForm />
+      </div>
+    </div>
+  );
+};
 
-export default Reviews
+export default Reviews;
 
 Reviews.propTypes = {
-  active: PropTypes.string
-}
+  active: PropTypes.string,
+  singleProduct: PropTypes.object,
+};
