7e8c56ae6c93dad368cba06b51f29499815f5f07
SBEGENJOV <seydi19981995@gmail.com>
2024-01-02 08:04:39 +0300

    Kupon kodu işlemleri gerçekleştirildi

--- frontend/src/context/CartProvider.jsx
+++ frontend/src/context/CartProvider.jsx
@@ -4,7 +4,11 @@ import PropTypes from "prop-types";
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState(localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : []);
+  const [cartItems, setCartItems] = useState(
+    localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : []
+  );
 
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
@@ -12,23 +16,27 @@ const CartProvider = ({ children }) => {
 
   const addToCart = (cartItem) => {
     // setCartItems([...cartItems, cartItem]); 1. yol
-    setCartItems((prevCart) => [...prevCart, {
-      ...cartItem,quantity:cartItem.quantity ? cartItem.quantity:1,
-    },
-  ]);
+    setCartItems((prevCart) => [
+      ...prevCart,
+      {
+        ...cartItem,
+        quantity: cartItem.quantity ? cartItem.quantity : 1,
+      },
+    ]);
   };
 
   const removeFromCart = (itemId) => {
-    const filteredCartItems =cartItems.filter((cartItem) => {
+    const filteredCartItems = cartItems.filter((cartItem) => {
       return cartItem._id !== itemId;
     });
-    setCartItems(filteredCartItems)
-  }
+    setCartItems(filteredCartItems);
+  };
 
   return (
     <CartContext.Provider
       value={{
         cartItems,
+        setCartItems,
         addToCart,
         removeFromCart,
       }}
