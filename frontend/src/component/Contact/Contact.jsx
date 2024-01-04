5ae51c5978e7496e8c20274a11d04ecc8d2c4e93
SBEGENJOV <seydi19981995@gmail.com>
2024-01-05 00:02:26 +0300

    İletişim sayfası soru gönderme yapıldı

--- frontend/src/component/Contact/Contact.jsx
+++ frontend/src/component/Contact/Contact.jsx
@@ -1,6 +1,39 @@
-import "./Contact.css"
+import { useState } from "react";
+import "./Contact.css";
+import { message } from "antd";
 
 const Contact = () => {
+  const [fromData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    content: "",
+  });
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...fromData, [name]: value });
+  };
+  const handleContact = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${apiUrl}/api/contact`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(fromData),
+      });
+      if (response.ok) {
+        message.success("Gönderi Başarılı");
+      } else {
+        message.success("Gönderi başarısız");
+      }
+    } catch (error) {
+      console.log("Gönderi hatalı: ", error);
+    }
+  };
+
   return (
     <section className="contact">
       <div className="contact-top">
@@ -21,73 +54,92 @@ const Contact = () => {
       <div className="contact-bottom">
         <div className="container">
           <div className="contact-titles">
-            <h4>Contact with us</h4>
-            <h2>Get In Touch</h2>
+            <h4>Bizimle iletişime geçin</h4>
+            <h2>Sorunu Bildir</h2>
             <p>
-              In hac habitasse platea dictumst. Pellentesque viverra sem nec
-              orci lacinia, in bibendum urna mollis. Quisque nunc lacus, varius
-              vel leo a, pretium lobortis metus. Vivamus consectetur consequat
-              justo.
+              Birbirimizi dinlemek ve anlamak, her birimizin kendi benzersiz
+              sesini duyurmasına olanak tanır. İletişim kurarken anlayış, saygı
+              ve empatiyle birbirimize yaklaşmak, güçlü ve anlamlı ilişkiler
+              kurmamızı sağlar.
             </p>
           </div>
           <div className="contact-elements">
-            <form className="contact-form">
+            <form className="contact-form" onSubmit={handleContact}>
               <div className="">
                 <label>
-                  Your Name
+                  İsminiz:
                   <span>*</span>
                 </label>
-                <input type="text" required />
+                <input
+                  type="text"
+                  name="name"
+                  required
+                  onChange={handleInputChange}
+                />
               </div>
               <div className="">
                 <label>
-                  Your email
+                  Mailiniz:
                   <span>*</span>
                 </label>
-                <input type="text" required />
+                <input
+                  type="text"
+                  name="mail"
+                  required
+                  onChange={handleInputChange}
+                />
               </div>
               <div className="">
                 <label>
-                  Subject
+                  Konu
                   <span>*</span>
                 </label>
-                <input type="text" required />
+                <input
+                  type="text"
+                  name="subject"
+                  required
+                  onChange={handleInputChange}
+                />
               </div>
               <div className="">
                 <label>
-                  Your message
+                  İçerik:
                   <span>*</span>
                 </label>
                 <textarea
                   id="author"
-                  name="author"
+                  name="content"
                   type="text"
                   defaultValue=""
                   size="30"
+                  maxLength={300}
                   required=""
+                  onChange={handleInputChange}
                 ></textarea>
               </div>
-              <button className="btn btn-sm form-button">Send Message</button>
+              <button className="btn btn-sm form-button">Yolla</button>
             </form>
             <div className="contact-info">
               <div className="contact-info-item">
                 <div className="contact-info-texts">
-                  <strong> Clotya Store</strong>
+                  <strong> Adresimiz</strong>
                   <p className="contact-street">
                     Clotya Store Germany — 785 15h Street, Office 478/B Green
                     Mall Berlin, De 81566
                   </p>
-                  <a href="tel:Phone: +1 1234 567 88">Phone: +1 1234 567 88</a>
+                  <a href="tel:Phone: +1 1234 567 88">Numara: +555 555 55 55</a>
                   <a href="mailto:Email: contact@example.com">
-                    Email: contact@example.com
+                    Email: iletişim@gmail.com
                   </a>
                 </div>
               </div>
               <div className="contact-info-item">
                 <div className="contact-info-texts">
-                  <strong> Opening Hours</strong>
-                  <p className="contact-date">Monday - Friday : 9am - 5pm</p>
-                  <p>Weekend Closed</p>
+                  <strong>Açık Oldugumuz Saatler</strong>
+                  <p className="contact-date">
+                    Pazartesi - Cuma : Sabah:9 - Akşam-5
+                  </p>
+                  <p>Hatfa Sonu Kapalı</p>
                 </div>
               </div>
             </div>
